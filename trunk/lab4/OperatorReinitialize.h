f9abb25fbf0f7725ac7c11dafff7f80f8c0243bd
onnepoika <onnepoika@da195381-492e-0410-b4d9-ef7979db4686>
2007-05-15 14:17:45 +0000

    update #235753

--- trunk/lab4/OperatorReinitialize.h
+++ trunk/lab4/OperatorReinitialize.h
@@ -116,6 +116,100 @@ public :
 
   }
 
+  virtual void propagateTo( float delta )
+	  {
+	  // Create buffer used to store intermediate results
+	  std::vector<float> buffer;
+
+	  // Determine timestep for stability
+	  float dx = mLS->getDx();
+	  float dt = 0.5 * dx;
+
+	  // Read maximum norm of gradient
+	  float maxGrad = std::numeric_limits<float>::max();
+
+	while( maxGrad>delta )
+	{
+
+	  // Propagate level set with stable timestep dt
+	  // until requested time is reached
+	  for (float elapsed = 0, time = 0.02; elapsed < time;) 
+		  {
+
+		  getGrid().dilate();
+
+		  if (dt > time-elapsed)
+			  dt = time-elapsed;
+		  elapsed += dt;
+
+		  // Iterate over grid and compute the grid values for the next timestep
+		  LevelSetGrid::Iterator iter = getGrid().beginNarrowBand();
+		  LevelSetGrid::Iterator iend = getGrid().endNarrowBand();
+		  while (iter != iend) {
+			  unsigned int i = iter.getI();
+			  unsigned int j = iter.getJ();
+			  unsigned int k = iter.getK();
+
+			  // Compute the sign function (from central differencing?)
+			  float ddxc = mLS->diffXpm(i,j,k);
+			  float ddyc = mLS->diffYpm(i,j,k);
+			  float ddzc = mLS->diffZpm(i,j,k);
+			  float normgrad2 = ddxc*ddxc + ddyc*ddyc + ddzc*ddzc;
+			  float val = getGrid().getValue(i,j,k);
+			  float sign = val / std::sqrt(val*val + normgrad2*dx*dx);
+
+			  float ddx2, ddy2, ddz2;
+			  godunov(i,j,k, sign, ddx2, ddy2, ddz2);
+
+			  // Compute the new value and store it in the buffer
+			  float ddt = sign * (1 - std::sqrt(ddx2 + ddy2 + ddz2));
+			  val = getGrid().getValue(i,j,k) + ddt*dt;
+			  buffer.push_back(val);
+
+			  iter++;
+			  }
+
+		  // Copy new values from buffer to grid
+		  iter = getGrid().beginNarrowBand();
+		  std::vector<float>::iterator iterBuffer = buffer.begin();
+		  while (iter != iend) {
+			  unsigned int i = iter.getI();
+			  unsigned int j = iter.getJ();
+			  unsigned int k = iter.getK();
+
+			  getGrid().setValue(i,j,k, (*iterBuffer));
+			  iter++;
+			  iterBuffer++;
+			  }
+		  buffer.clear();
+
+		  // Read maximum norm of gradient
+		  maxGrad = -std::numeric_limits<float>::max();
+		  iter = getGrid().beginNarrowBand();
+		  while (iter != iend) {
+			  unsigned int i = iter.getI();
+			  unsigned int j = iter.getJ();
+			  unsigned int k = iter.getK();
+
+
+			  float ddxc = mLS->diffXpm(i,j,k);
+			  float ddyc = mLS->diffYpm(i,j,k);
+			  float ddzc = mLS->diffZpm(i,j,k);
+			  float normgrad2 = ddxc*ddxc + ddyc*ddyc + ddzc*ddzc;
+			  if (maxGrad < normgrad2) maxGrad = normgrad2;
+
+			  iter++;
+			  }
+		  maxGrad = std::sqrt(maxGrad);
+		  std::cerr << "Maximum gradient: " << maxGrad << std::endl;
+
+		  getGrid().rebuild();
+		  }
+		}
+
+
+	  }
+
 };
 
 #endif
