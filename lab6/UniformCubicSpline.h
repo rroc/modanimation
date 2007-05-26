7d8a900f6715d98a71c00da0f6a5a44a3fdc5a49
onnepoika <onnepoika@da195381-492e-0410-b4d9-ef7979db4686>
2007-05-26 17:46:29 +0000

    mesh subdivision and optimization of the analytical cubic spline "optimization"

--- lab6/UniformCubicSpline.h
+++ lab6/UniformCubicSpline.h
@@ -1,14 +1,14 @@
 /*************************************************************************************************
- *
- * Modeling and animation (TNM079) 2007
- * Code base for lab assignments. Copyright:
- *   Gunnar Johansson (gunnar.johansson@itn.liu.se)
- *   Ken Museth (ken.museth@itn.liu.se)
- *   Michael Bang Nielsen (bang@daimi.au.dk)
- *   Ola Nilsson (ola.nilsson@itn.liu.se)
- *   Andreas Söderström (andreas.soderstrom@itn.liu.se)
- *
- *************************************************************************************************/
+*
+* Modeling and animation (TNM079) 2007
+* Code base for lab assignments. Copyright:
+*   Gunnar Johansson (gunnar.johansson@itn.liu.se)
+*   Ken Museth (ken.museth@itn.liu.se)
+*   Michael Bang Nielsen (bang@daimi.au.dk)
+*   Ola Nilsson (ola.nilsson@itn.liu.se)
+*   Andreas Söderström (andreas.soderstrom@itn.liu.se)
+*
+*************************************************************************************************/
 #ifndef __uniform_cubic_spline_h__
 #define __uniform_cubic_spline_h__
 
@@ -20,102 +20,121 @@
 
 class UniformCubicSpline : public Geometry {
 protected:
-  //! The coefficients dictating the shape
-  std::vector<Vector3<float> > mCoefficients;
-  //! The control polygon is simply a LineStrip
-  LineStrip mControlPolygon;
+	//! The coefficients dictating the shape
+	std::vector<Vector3<float> > mCoefficients;
+	//! The control polygon is simply a LineStrip
+	LineStrip mControlPolygon;
 
-  //! Decides the length of the linear approximating segments used when drawing
-  float mDt;
+	//! Decides the length of the linear approximating segments used when drawing
+	float mDt;
 
-  // display information
-  Vector3<float> mLineColor;
-  GLfloat mLineWidth;
+	// display information
+	Vector3<float> mLineColor;
+	GLfloat mLineWidth;
 
 public:
-  UniformCubicSpline(const std::vector<Vector3<float> > &joints,
-		     Vector3<float> lineColor = Vector3<float>(0.f,1.f,0.f),
-		     GLfloat lineWidth = 2.f,
-		     float segmentLength = .01f) : mCoefficients(joints),
-						   mControlPolygon(joints){
-    this->mLineColor = lineColor;
-    this->mLineWidth = lineWidth;
-    this->mDt = segmentLength;
-  }
+	UniformCubicSpline(const std::vector<Vector3<float> > &joints,
+		Vector3<float> lineColor = Vector3<float>(0.f,1.f,0.f),
+		GLfloat lineWidth = 2.f,
+		float segmentLength = .01f) : mCoefficients(joints),
+		mControlPolygon(joints)
+		{
+		this->mLineColor = lineColor;
+		this->mLineWidth = lineWidth;
+		this->mDt = segmentLength;
+		}
 
-  /*! The BSpline value is calculated from one of the four cardinal BSpline
-   * segments
-   */
-  float getBSplineValue(int i, float t) const {
-    // Find the offset from spline i
-    t = t - (float)i;
-    if(std::abs(t) >= 2){
-      // if outside of support return zero
-      return 0;
-    } else if(0 <= t && t < 1)
-      {
-	// get the fractional part
-	float ft = t;
-	return 1/6.0*(3*ft*ft*ft - 6*ft*ft + 4);
-      }
-    else if(1 <= t && t < 2)
-      {
-	// get the fractional part
-	float ft = t - 1;
-	return 1/6.0*(-ft*ft*ft + 3*ft*ft - 3*ft + 1);
-      }
-    else if(-1 <= t && t < 0)
-      {
-	// get the fractional part
-	float ft = t + 1;
-	return 1/6.0*(-3*ft*ft*ft + 3*ft*ft + 3*ft + 1);
-      }
-    else if(-2 < t && t < -1)
-      {
-	// get the fractional part
-	float ft = t + 2;
-	return 1/6.0*(ft*ft*ft);
-      }
-    return 0;
-  }
+	/*! The BSpline value is calculated from one of the four cardinal BSpline
+	* segments
+	*/
+	float getBSplineValue(int i, float t) const 
+		{
+		// Find the offset from spline i
+		t = t - (float)i;
+		if(std::abs(t) >= 2)
+			{
+			// if outside of support return zero
+			return 0;
+			} 
+		else if(0 <= t && t < 1)
+			{
+			// get the fractional part
+			float ft = t;
+			return 1/6.0*(3*ft*ft*ft - 6*ft*ft + 4);
+			}
+		else if(1 <= t && t < 2)
+			{
+			// get the fractional part
+			float ft = t - 1;
+			return 1/6.0*(-ft*ft*ft + 3*ft*ft - 3*ft + 1);
+			}
+		else if(-1 <= t && t < 0)
+			{
+			// get the fractional part
+			float ft = t + 1;
+			return 1/6.0*(-3*ft*ft*ft + 3*ft*ft + 3*ft + 1);
+			}
+		else if(-2 < t && t < -1)
+			{
+			// get the fractional part
+			float ft = t + 2;
+			return 1/6.0*(ft*ft*ft);
+			}
+		return 0;
+		}
 
-  /*! Evaluate the spline as the sum of the coefficients times the bsplines */
-  Vector3<float> getValue(float t) const {
-    Vector3<float> val;
-    float sum = 0;
-    for(std::vector<Vector3<float> >::const_iterator it(mCoefficients.begin());
-	it!=mCoefficients.end();
-	it++){
-      // it - begin() is gives the distance between the two iterators which
-      // can be used as a linear index
-      float bval = getBSplineValue(it-mCoefficients.begin(), t);
-      val += (*it)*bval;
-      sum += bval;
-    }
-    return val;
-  }
+	/*! Evaluate the spline as the sum of the coefficients times the bsplines */
+	Vector3<float> getValue(float t) const 
+		{
+		Vector3<float> val;
+		float sum = 0;
+		bool outOfRange = true;
+		for(std::vector<Vector3<float> >::const_iterator it(mCoefficients.begin());
+			it!=mCoefficients.end();
+			it++)
+			{
+			// it - begin() is gives the distance between the two iterators which
+			// can be used as a linear index
+			float bval = getBSplineValue(it-mCoefficients.begin(), t);
 
-  virtual void draw(){
+			if(0!=bval && outOfRange )
+				{
+				outOfRange=false;
+				}
 
-    mControlPolygon.draw();
+			//If a valid number already got, and now reached 0
+			if( !outOfRange && 0.0f == bval )
+				{
+				break;
+				}
+			val += (*it)*bval;
+			sum += bval;
+			}
+		return val;
+		}
 
-    // save line point and color states
-    glPushAttrib(GL_POINT_BIT | GL_LINE_BIT | GL_CURRENT_BIT);
+	virtual void draw(){
 
-    // draw segments
-    glLineWidth(mLineWidth);
-    glColor3fv(mLineColor.getArrayPtr());
-    glBegin(GL_LINE_STRIP);
+		mControlPolygon.draw();
 
-    // We only have full BSpline support from spline at index 1, thus we begin evaluating at 1.0
-    for(float i = 1; i < mCoefficients.size()-2; i+= mDt){
-      glVertex3fv( this->getValue(i).getArrayPtr());
-    }
-    glEnd();
+		// save line point and color states
+		glPushAttrib(GL_POINT_BIT | GL_LINE_BIT | GL_CURRENT_BIT);
 
-    // restore attribs
-    glPopAttrib();
+		// draw segments
+		glLineWidth(mLineWidth);
+		glColor3fv(mLineColor.getArrayPtr());
+		glBegin(GL_LINE_STRIP);
 
-  }
-};
+		// We only have full BSpline support from spline at index 1, thus we begin evaluating at 1.0
+		for(float i = 1; i < mCoefficients.size()-2; i+= mDt)
+			{
+			glVertex3fv( this->getValue(i).getArrayPtr() );
+			}
+		glEnd();
+
+		// restore attribs
+		glPopAttrib();
+
+		}
+	};
 #endif
