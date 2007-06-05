0427ec597d6590e57034c7d36aca6d308b69b841
jpjorge <jpjorge@da195381-492e-0410-b4d9-ef7979db4686>
2007-06-05 08:48:57 +0000

    corrected the getValue

--- trunk/lab6/UniformCubicSpline.h
+++ trunk/lab6/UniformCubicSpline.h
@@ -38,80 +38,133 @@ public:
 		GLfloat lineWidth = 2.f,
 		float segmentLength = .01f) : mCoefficients(joints),
 		mControlPolygon(joints)
-		{
+	{
 		this->mLineColor = lineColor;
 		this->mLineWidth = lineWidth;
 		this->mDt = segmentLength;
-		}
+	}
 
 	/*! The BSpline value is calculated from one of the four cardinal BSpline
 	* segments
 	*/
 	float getBSplineValue(int i, float t) const 
-		{
+	{
 		// Find the offset from spline i
 		t = t - (float)i;
 		if(std::abs(t) >= 2)
-			{
+		{
 			// if outside of support return zero
 			return 0;
-			} 
+		} 
 		else if(0 <= t && t < 1)
-			{
+		{
 			// get the fractional part
 			float ft = t;
 			return 1/6.0*(3*ft*ft*ft - 6*ft*ft + 4);
-			}
+		}
 		else if(1 <= t && t < 2)
-			{
+		{
 			// get the fractional part
 			float ft = t - 1;
 			return 1/6.0*(-ft*ft*ft + 3*ft*ft - 3*ft + 1);
-			}
+		}
 		else if(-1 <= t && t < 0)
-			{
+		{
 			// get the fractional part
 			float ft = t + 1;
 			return 1/6.0*(-3*ft*ft*ft + 3*ft*ft + 3*ft + 1);
-			}
+		}
 		else if(-2 < t && t < -1)
-			{
+		{
 			// get the fractional part
 			float ft = t + 2;
 			return 1/6.0*(ft*ft*ft);
-			}
-		return 0;
 		}
+		return 0;
+	}
+
+	inline Vector3<float> evaluateBSpline(float t) const
+	{
+		int knot = (int)t;
+		Vector3<float> coef, sum;
+		float bSplineVal;
+
+		// iterate from max(0, knot) 
+		int i = std::max(knot - 2, 0);
+		for (; i < knot+3; i++)
+		{
+			coef = mCoefficients[ i ];
+			bSplineVal = getBSplineValue(i, t);
+			
+			//printf("bSplineValue: %f\n", bSplineVal);
+
+			Vector3<float> vr = coef * bSplineVal;
+			//printf("Val: [%f, %f, %f] \n", vr.x(), vr.y(), vr.z());
+
+			sum += coef * bSplineVal;
+		}
+
+		return sum;
+	}
 
 	/*! Evaluate the spline as the sum of the coefficients times the bsplines */
 	Vector3<float> getValue(float t) const 
+	{
+		// get the corresponding knot
+		int knot = (int)t;
+		Vector3<float> coef, sum;
+		float bSplineVal;
+
+		// iterate from max(0, knot) 
+		int i = std::max(knot - 2, 0);
+		for (; i < knot+3; i++)
 		{
-		Vector3<float> val;
-		float sum = 0;
-		bool outOfRange = true;
-		for(std::vector<Vector3<float> >::const_iterator it(mCoefficients.begin());
-			it!=mCoefficients.end();
-			it++)
-			{
-			// it - begin() is gives the distance between the two iterators which
-			// can be used as a linear index
-			float bval = getBSplineValue(it-mCoefficients.begin(), t);
+			coef = mCoefficients[ i ];
+			bSplineVal = getBSplineValue(i, t);
 
-			if(0!=bval && outOfRange )
-				{
-				outOfRange=false;
-				}
-
-			//If a valid number already got, and now reached 0
-			if( !outOfRange && 0.0f == bval )
-				{
-				break;
-				}
-			val += (*it)*bval;
-			sum += bval;
-			}
-		return val;
+			Vector3<float> vr = coef * bSplineVal;
+			sum += coef * bSplineVal;
 		}
+
+		return sum;
+
+		//////////////////////////////////////////////////////////////////////////
+		// OLD VERSION
+		//////////////////////////////////////////////////////////////////////////
+		//Vector3<float> v = evaluateBSpline( t );
+		//printf("--------------------\n");
+
+		//Vector3<float> val;
+		//float sum = 0;
+		//bool outOfRange = true;
+		//for(std::vector<Vector3<float> >::const_iterator it(mCoefficients.begin());
+		//	it!=mCoefficients.end();
+		//	it++)
+		//{
+		//	// it - begin() is gives the distance between the two iterators which
+		//	// can be used as a linear index
+		//	float bval = getBSplineValue(it-mCoefficients.begin(), t);
+
+		//	if(0!=bval && outOfRange )
+		//	{
+		//		outOfRange=false;
+		//	}
+
+		//	//If a valid number already got, and now reached 0
+		//	if( !outOfRange && 0.0f == bval )
+		//	{
+		//		break;
+		//	}
+
+		//	Vector3<float> vr = (*it)*bval;
+		//	printf("Val: [%f, %f, %f] \n", vr.x(), vr.y(), vr.z());
+
+		//	val += (*it)*bval;
+		//	sum += bval;
+		//}
+		//printf("--------------------\n");
+		//return val;
+	}
 
 	virtual void draw(){
 
@@ -127,14 +180,14 @@ public:
 
 		// We only have full BSpline support from spline at index 1, thus we begin evaluating at 1.0
 		for(float i = 1; i < mCoefficients.size()-2; i+= mDt)
-			{
+		{
 			glVertex3fv( this->getValue(i).getArrayPtr() );
-			}
+		}
 		glEnd();
 
 		// restore attribs
 		glPopAttrib();
 
-		}
-	};
+	}
+};
 #endif
